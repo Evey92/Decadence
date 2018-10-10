83c1e556857fd418e3839555735c913a437152cd
Emilio <idv18c.esolis@uartesdigitales.edu.mx>
2018-10-10 15:31:50 -0500

    Finally decided to make breakable objects.

---
+++ Decadence/Assets/Scripts/Attack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+  private float m_damage;
+  private BreakObject m_breakScript;
+
+  // Use this for initialization
+  void Start()
+  {
+    m_damage = 1;
+    m_breakScript = null;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // If m_breakScript isn't null and Q key is pressed GetDamage is called
+    if(m_breakScript) {
+      if (Input.GetKeyDown(KeyCode.Q)) {
+        m_breakScript.GetDamage(m_damage);
+        m_breakScript = null;
+      }
+    }
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    // If the player touches a breakable object it gets the script so it can call GetDamage
+    if(collision.gameObject.tag=="Breakable") {
+      m_breakScript = collision.gameObject.GetComponent<BreakObject>();
+    }
+  }
+
+  private void OnCollisionExit(Collision collision)
+  {
+    // If the player is no longer in contact with a breakable object m_breakScript is null
+    m_breakScript = null;
+  }
+}
---
+++ Decadence/Assets/Scripts/BreakObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;                                                                     
+using System.Collections.Generic;                                                             
+using UnityEngine;
+
+public class BreakObject : MonoBehaviour
+{
+  // Breakable object m_hp                                                                      
+  public GameObject m_brokenObject;
+  private float m_hp;
+
+  // Use this for initialization                                                              
+  void Start()
+  {
+    // Sets m_hp to 1, that means any kind of damage will break the object                      
+    m_hp = 1;
+  }
+
+  // Update is called once per frame                                                          
+  void Update()
+  {
+    // Check if m_hp reaches 0                                                                  
+    if (m_hp <= 0) {
+      // If it does, the broken object is instatiated in its place and this one gets destroyed
+      Instantiate(m_brokenObject, this.transform.position, this.transform.rotation);
+      Destroy(gameObject);
+    }
+  }
+
+  public void GetDamage(float damage)
+  {
+    m_hp -= damage;
+  }
+}
