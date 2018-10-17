610b747d6b4dd15f4b64b868fcdae4e299b8df91
Emilio <idv18c.esolis@uartesdigitales.edu.mx>
2018-10-17 13:16:36 -0500

    *Ten Jalil

---
+++ Decadence/Assets/DisableCanvas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCanvas : MonoBehaviour {
+    [SerializeField]
+    public Canvas c;
+	// Use this for initialization
+	void Start () {
+        c = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            c.enabled = !c.enabled;
+        }
+	}
+}
