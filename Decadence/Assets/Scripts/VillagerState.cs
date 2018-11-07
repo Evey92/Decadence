1433f64390ba4b9d280f2b14c883b1e0eabf24de
Alberto Villa <idv16c.jvilla@uartesdigitales.edu.mx>
2018-11-07 14:14:19 -0600

    * Holi, este es un commit bien risky alv

---
+++ Decadence/Assets/Scripts/VillagerState.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerState
+{
+  public virtual void handleInput(Villager villager)
+  {
+
+  }
+
+  public virtual void update(Villager villager, float deltaTime)
+  {
+
+  }
+}
+
+public class IdleState : VillagerState
+{
+  private float time;
+
+  public IdleState()
+  {
+    time = 2.0f;
+  }
+
+  public override void handleInput(Villager villager)
+  {
+
+  }
+
+  public override void update(Villager villager, float deltaTime)
+  {
+    time -= deltaTime;
+    Debug.Log(time);
+    if (time <= 0)
+    {
+      villager.updateState(new WanderState());
+      villager.run();
+    }
+  }
+}
+
+public class WanderState : VillagerState
+{
+  private float time;
+
+  public WanderState()
+  {
+    time = 5.0f;
+  }
+
+  public override void handleInput(Villager villager)
+  {
+
+  }
+
+  public override void update(Villager villager, float deltaTime)
+  {
+    time -= deltaTime;
+    Debug.Log(time);
+    if (time <= 0)
+    {
+      villager.updateState(new IdleState());
+      villager.idle();
+    }
+  }
+}
