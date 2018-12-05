9cb17af126fb12be56a995a171908bcfbd298fce
jalilraziel <idv16c.jzavala@uartesdigitales.edu.mx>
2018-12-05 10:23:03 -0600

    mapa blocking

---
+++ Decadence/Assets/NumEfeccts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumEfeccts : MonoBehaviour {
+    public Ojos_de_caballero ojos;
+    public int attack;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        ojos.numeffects = attack;
+        Debug.Log("cualquier cosa");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Decadence/Assets/Scripts/Ojos_de_caballero.cs
+++ Decadence/Assets/Scripts/Ojos_de_caballero.cs
@@ -5,25 +5,27 @@ using System.Threading;
 
 public class Ojos_de_caballero : MonoBehaviour
 {
+    private bool ObjectInCamare;
+    //private GameObject Cosa;
+    public int numeffects = 0;
     public GameObject[] efectos;
-    public int effects = 0;
     bool trasparentar = false;
     // Use this for initialization
     void Start()
     {
-
+      //  Cosa = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        ObjectInCamare = false;
     }
 
     // Modifica la opacidad a 20 frames por segundo
-    IEnumerator Opacidad()
-    {
+    IEnumerator Opacidad() {
         //Asigno el contenio del color a c para modificarlo:
-        Color c = efectos[effects].GetComponent<SpriteRenderer>().color;
+        Color c = efectos[numeffects].GetComponent<SpriteRenderer>().color;
         //Aparece la imagen en .5s en 10 frama
         for (float f = 0; f < 1; f += .1f)
         {
             c.a = f;
-            efectos[effects].GetComponent<SpriteRenderer>().color = c;
+            efectos[numeffects].GetComponent<SpriteRenderer>().color = c;
             yield return new WaitForSeconds(.05f);
         }
         //Duracion de imagen a 100% de opacidad
@@ -32,43 +34,58 @@ public class Ojos_de_caballero : MonoBehaviour
         for (float f = 1; f > 0; f -= .05f)
         {
             c.a = f;
-            efectos[effects].GetComponent<SpriteRenderer>().color = c;
+            efectos[numeffects].GetComponent<SpriteRenderer>().color = c;
             yield return new WaitForSeconds(.05f);
         }
 
         //Contador super improvisado
-        effects++;
-        if (effects > efectos.Length) effects = 0;
-        //Desactivamos el efecto y luego el objeto
+       
+        //Desactivamos el efecto
         trasparentar = false;
-        efectos[effects].SetActive(false);
     }
 
     //Trigger para iniciar la corrutina de "opacidad()"
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other) {
         //Revision de animacion de trasparentar:
+        Debug.Log("cualquier cosa2");
         if (trasparentar == false)
         {
             //Activamos el objeto con el efecto, despues el seguro de este, de ultimo el efecto.   
-            efectos[effects].SetActive(true);
+            efectos[numeffects].SetActive(true);
             trasparentar = true;
             StartCoroutine("Opacidad");
+            efectos[numeffects].SetActive(false);
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //scriptmagico ElScript = GetComponent<scriptmagico>();
-        //NumEffects = ElScript.VariableAUsar 
-
+    void Update(){
         //Activa efecto asignado con "q"
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            efectos[effects].SetActive(true);
+            efectos[numeffects].SetActive(true);
             //efecto asignado:
             StartCoroutine("Opacidad");
         }
+
+      //if (Input.GetKeyDown(KeyCode.Z)){
+      //      ViewObject(Cosa);
+      //  }
     }
+
+    //void ViewObject(GameObject obj)
+    //{
+    //        GameObject InspectedObject;
+    //        InspectedObject = obj;
+
+    //        Vector3 CenterCamara = new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane);
+    //        InspectedObject.transform.position = Camera.main.ScreenToWorldPoint(CenterCamara);
+    //        if (ObjectInCamare == false)
+    //        {
+    //            InspectedObject.SetActive(true);
+    //        }
+    //        else{
+    //            InspectedObject.SetActive(false);
+    //        }
+    //}
 }
